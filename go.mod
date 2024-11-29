eab845a8db577d7a439b73748170a20115b3aa2e
Mohammad Mahdi Samiei <mohammadmahdi.samiei@divar.ir>
2024-11-29 21:46:20 +0330

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/mmsamiei/golang-telegram-bot
+
+go 1.23.1
+
+require github.com/go-telegram/bot v1.11.1 // indirect
---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"context"
+	"os"
+	"os/signal"
+
+	"github.com/go-telegram/bot"
+	"github.com/go-telegram/bot/models"
+)
+
+func main() {
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
+
+	opts := []bot.Option{
+		bot.WithDefaultHandler(handler),
+	}
+
+	botToken := os.Getenv("YOUR_BOT_TOKEN_FROM_BOTFATHER")
+	b, err := bot.New(botToken, opts...)
+	if err != nil {
+		panic(err)
+	}
+
+	b.Start(ctx)
+}
+
+func handler(ctx context.Context, b *bot.Bot, update *models.Update) {
+	b.SendMessage(ctx, &bot.SendMessageParams{
+		ChatID: update.Message.Chat.ID,
+		Text:   "I echo " + update.Message.Text,
+	})
+}
